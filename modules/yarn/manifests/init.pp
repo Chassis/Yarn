01aa73ebd7e17f4be0da456e78c5d7390ab33960
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-14 15:23:56 +1000

    Fix linting issues

--- modules/yarn/manifests/init.pp
+++ modules/yarn/manifests/init.pp
@@ -1,17 +1,18 @@
+# A Chassis extension to install yarn on your Chassis Box.
 class yarn (
 	$config,
-	$path = "/vagrant/extensions/yarn",
+	$path = '/vagrant/extensions/yarn',
 ) {
 	if ( ! empty($config[disabled_extensions]) and 'chassis/yarn' in $config[disabled_extensions] ) {
 		exec { 'uninstall yarn':
-			path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-			command     => 'npm uninstall -g yarn',
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => 'npm uninstall -g yarn',
 		}
 	} else {
 		exec { 'install yarn':
-			path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-			command     => 'npm install -g yarn',
-			require     => [ Class['nodejs'] ],
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => 'npm install -g yarn',
+			require => [ Class['nodejs'] ],
 			unless  => 'which yarn',
 		}
 	}
