662d8bcf69618ed8c470e482f9a701a7ae33730d
Bronson Quick <bronson@bronsonquick.com.au>
2019-06-17 15:24:44 +1000

    Fix spacing

--- modules/yarn/manifests/init.pp
+++ modules/yarn/manifests/init.pp
@@ -10,10 +10,10 @@ class yarn (
 		}
 	} else {
 		exec { 'install yarn':
-			path     => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-			command  => 'npm install -g yarn',
-			require  => [ Class['nodejs'] ],
-			unless   => 'which yarn',
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => 'npm install -g yarn',
+			require => [ Class['nodejs'] ],
+			unless  => 'which yarn',
 		}
 
 		if ( $config[yarn] and $config[yarn][paths]) {
